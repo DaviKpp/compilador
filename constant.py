a3bb45d03378e8fe41833554b8634503428e783b
Davi Kreppel <66695188+DaviKpp@users.noreply.github.com>
2022-04-26 11:48:17 -0300

    Add files via upload

--- constant.py
+++ constant.py
@@ -4,10 +4,10 @@ import re
 inteiro = "^[-+]?[0-9]+$"
 alfabeto = "[a-zA-Z]"
 palavrarese = "(void|main|printf|for|while|do|if|else|int|float|char|double|long)"
-delimitador = "(\s|\n|+|-|,|{|}|(|)|;|/|[|]|=|*|<|>)"
+delimitador = "[\s|\n|+|-|,|{|}|(|)|;|/|=|*|<|>]"
 deli = "[\s|(|)|;|+|=|{|}]"
 operador = "[+|-|=|*|/|<|>]"
-
+literal = "['a-zA-Z']"
 
 class Token:
     def __init__(self, lexema, tipo, linha, coluna, pos, erro):
@@ -24,35 +24,26 @@ class Token:
         "\nTipo: " + str(self.tipo) + 
         "\nLinha: " + str(self.linha) + 
         "\nColuna: " + str(self.coluna) + 
-        "\nPosição: " + str(self.pos) +
         "\nErro: " + str(self.erro))
 
-def crawler(arq, regex, sentinela, scout, tipo, arrToken):
-    linha = 0
-    coluna = 0
+def crawler(arq, regex, sentinela, scout, tipo, arrToken, linha, coluna):
     pos = 0
     if re.match(regex, arq[scout]):
         sentinela = scout
-        print(arq[scout])
         scout = scout + 1
         while scout < len(arq) and re.match(regex, arq[scout]):
-            print(arq[scout])
             scout = scout + 1
         if scout == len(arq):
             scout = scout - 1 
         if not re.match(deli, arq[scout]):
-            print("Erro Léxico!")
             lexema = ''.join([str(elem) for elem in arq[sentinela:scout]])
             token = Token(lexema, tipo, linha, coluna, pos, 1)
-            token.printToken()
             arrToken.append(token)
             pos = pos + 1
             return sentinela, scout
         else:
-            print("Token Criado!")
             lexema = ''.join([str(elem) for elem in arq[sentinela:scout]])
             token = Token(lexema, tipo, linha, coluna, pos, 0)
-            token.printToken()
             arrToken.append(token)
             pos = pos + 1
             return sentinela, scout
@@ -61,7 +52,6 @@ def crawler(arq, regex, sentinela, scout, tipo, arrToken):
 
 def faztoken(arq, tipo, arrToken, scout, linha, coluna):
        token = Token(arq[scout], tipo, linha, coluna, 0, 0)
-       token.printToken()
        arrToken.append(token)
 
 def simplecrawler(arq, sentinela, scout, arrToken, linha, coluna):
--- lexico.py
+++ lexico.py
@@ -12,30 +12,41 @@ scout = 0
 arrToken = []
 espaco = " "
 r = r + espaco
-linha = 0
-coluna = 0
+linha = 1
+coluna = 1
 while scout < len(r):
     temp = scout
-
+    if(r[scout]== '\n'):
+        linha = linha + 1
+        coluna = 1
     #Operador
     constant.simplecrawler(r, sentinela, scout, arrToken, linha, coluna)
 
     #Inteiros
-    sentinela, scout = constant.crawler(r, constant.inteiro, sentinela, scout, 'inteiro', arrToken)
+    sentinela, scout = constant.crawler(r, constant.inteiro, sentinela, scout, 'inteiro', arrToken, linha, coluna)
+
+    #Literal
+    sentinela, scout = constant.crawler(r, constant.literal, sentinela, scout, 'literal', arrToken, linha, coluna)
     
     #Id
-    sentinela, scout = constant.crawler(r, constant.alfabeto, sentinela, scout, 'id', arrToken)
-    
+    sentinela, scout = constant.crawler(r, constant.alfabeto, sentinela, scout, 'id', arrToken, linha, coluna)
+
+
     if scout == temp:
-        
-        
-        
+        if not re.match(constant.delimitador, r[scout]):
+            print("Erro léxico!\n"+
+            "Linha:" + str(linha) +
+            "\nColuna: " + str(coluna))
         scout = scout + 1
+    else:
+        coluna = coluna+1
 f = open('lexico.txt', 'w')
 for x in arrToken:
     
     if x.erro == 1:
-        print("Erro léxico")
+        print("Erro léxico!\n"+
+        "Linha:" + str(x.linha) +
+        "\nColuna: " + str(x.coluna))
     else:
         if re.match(constant.palavrarese, x.lexema):
             x.tipo = 'palavra-reservada'
